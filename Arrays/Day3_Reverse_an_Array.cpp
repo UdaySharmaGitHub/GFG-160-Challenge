1e2d8c0b58b7911ba41f85d2824e0f36cea16e7c
Uday <udaysharma4600@gmail.com>
2024-11-20 22:03:03 +0530

    Day-3 Reverse an Array

---
+++ Arrays/Day3_Reverse_an_Array.cpp
@@ -0,0 +1,25 @@
+/*
+    Approach-1 (without Using In-Built Function)
+    Time Complexity: O(n)
+    Space Complexity: O(1)
+*/
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        reverse(arr.begin(),arr.end());
+    }
+};
+/*
+    Approach-2 (Using In-Built Function)
+    Time Complexity O(n)
+    Space Complexity O(1)
+*/
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        reverse(arr.begin(),arr.end());
+    }
+};
+
