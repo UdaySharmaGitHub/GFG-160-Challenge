56bddacc60894654d58b36d12935f3bdc797dec0
Uday <udaysharma4600@gmail.com>
2024-11-20 22:03:48 +0530

    Day-5 Next Permutation

---
+++ Arrays/Day5_Next_Permutation.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        next_permutation(arr.begin(), arr.end());
+    }
+};
